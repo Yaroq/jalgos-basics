32003e4942e36780034a1ea133e71991f8167def
yaroq <julien.muresianu@gmail.com>
2015-03-02 13:12:30 +0100

    simple implementation of merge sort

--- src/jalgos_basics/core.clj
+++ src/jalgos_basics/core.clj
@@ -1,6 +1,24 @@
 (ns jalgos-basics.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn mrg
+  "Input: 3 collections (supposed to be used on already ordered collections for the first 2 ones)
+   Output: 1 collections containing all elements of inputs in ascending order (if inputs are ordered)"
+  [left right final]
+  (loop [L left
+         R right
+         F final]
+    (if (and (not-empty L) (not-empty R))
+      (if (<= (first L) (first R))
+        (recur (rest L) R (conj F (first L)))
+        (recur (rest R) L (conj F (first R))))
+      (concat final left right))))
+
+
+(defn mrg-sort
+  "Input: a collection (should work with a string as well)
+   Output: vector of 2 collections approximately first half and second half of input"
+  [coll]
+  (if (> (count coll) 1)
+    (let [sides (split-at (/ (count coll) 2) coll)]
+      (mrg (mrg-sort (get sides 0)) (mrg-sort (get sides 1)) []))
+    coll))
